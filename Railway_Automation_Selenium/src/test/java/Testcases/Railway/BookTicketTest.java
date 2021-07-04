c7e393bde04cc46cd9cda675c6324914e60d50ef
dotrinh81197 <dotrinh81197@gmail.com>
2021-07-04 19:21:15 +0700

    update

---
+++ Railway_Automation_Selenium/src/test/java/Testcases/Railway/BookTicketTest.java
@@ -0,0 +1,35 @@
+package Testcases.Railway;
+
+import Common.Common.Utilities;
+import Common.Constant.Constant;
+import PageObjects.Railway.HomePage;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class BookTicketTest {
+    HomePage homePage = new HomePage();
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.setProperty("webdriver.chrome.driver", Utilities.getProjectPath());
+        Constant.WEBDRIVER = new ChromeDriver();
+        System.out.println("Pre-condition");
+        homePage.open();
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("Post-condition");
+        homePage.logout();
+    }
+
+    @Test(description = "Login page displays when un-logged User clicks on \"Book ticket\" tab")
+    public void TC04() {
+        homePage.gotoBookTicketPage();
+        Assert.assertTrue(homePage.isAtLoginPage(), "Login Page is not displayed");
+    }
+
+}
